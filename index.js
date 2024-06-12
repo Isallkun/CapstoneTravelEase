437e64f64ac709dab951682e325e85ca93e40079
faraclara <93864944+faraclara@users.noreply.github.com>
2024-06-13 04:03:27 +0700

    two commit

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ app.get('/', (req, res) => {
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
-app.listen(process.env.PORT || 8000, ()=> console.log(`Server running on port 8000`));
+app.listen(process.env.PORT || 3000, ()=> console.log(`Server running on port 3000`));
